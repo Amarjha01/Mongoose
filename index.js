d87f7aef4e663bdc3e28d2bf5deed5add4c7b65f
Amar <amarjha880@gmail.com>
2024-03-22 09:35:27 +0530

    nodemon added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+/* This JavaScript code snippet is using the Mongoose library to interact with a MongoDB database.
+Here's a breakdown of what the code is doing: */
 const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/e-com");
+const ProductSchema = new mongoose.Schema({ Name: String, price: Number , Brand: String});
 
-const main = async () => {
-    await mongoose.connect("mongodb://localhost:27017/e-com");
-    const ProductSchema = new mongoose.Schema({ Name: String, price: Number });
-
-    const ProductModel = mongoose.model('product', ProductSchema);
-    let data = new ProductModel({ Name: "iPhone 12", price: 1300 });
+const IncertINDB = async () => {
+    const ProductModel = mongoose.model('mobiles', ProductSchema);
+    let data = new ProductModel({ Name: "iPhone 16 pro max", price: 2200 , Brand: 'Apple' });
     data = await data.save();
+    
+    console.log('data has sucess fully IncertINDB :' , data);
 }
 
-main();
+
